3dfd2a16fa59bc729da56a53d8467d4de1ad74fb
Simon Pilkington <simonpi@amazon.com>
2018-12-05 04:26:02 -0800

    Add HTTPResponseOutputProtocol.

--- Sources/SmokeHTTPClient/HTTPClient.swift
+++ Sources/SmokeHTTPClient/HTTPClient.swift
@@ -34,7 +34,7 @@ public class HTTPClient {
     /// The connection timeout in seconds
     public let connectionTimeoutSeconds: Int
     
-    private static let unexpectedClosureType =
+    static let unexpectedClosureType =
         HTTPError.connectionError("Http request was unexpectedly closed without returning a response.")
 
     /// The event loop used by requests/responses from this client
@@ -76,311 +76,20 @@ public class HTTPClient {
             Log.error("Unable to shut down event loop group: \(error)")
         }
     }
-
+    
     /**
-     Submits a request that will return a response body to this client asynchronously.
-     The completion handler's execution will be scheduled on DispatchQueue.global()
-     rather than executing on a thread from SwiftNIO.
-
-     - Parameters:
-     - endpointPath: The endpoint path for this request.
-     - httpMethod: The http method to use for this request.
-     - input: the input body data to send with this request.
-     - completion: Completion handler called with the response body or any error.
-     - handlerDelegate: the delegate used to customize the request's channel handler.
+     Gracefully shuts down the eventloop if owned by this client.
      */
-    public func executeAsyncWithOutput<InputType, OutputType>(
-            endpointOverride: URL? = nil,
-            endpointPath: String,
-            httpMethod: HTTPMethod,
-            input: InputType,
-            completion: @escaping (HTTPResult<OutputType>) -> (),
-            handlerDelegate: HTTPClientChannelInboundHandlerDelegate) throws -> Channel
-        where InputType: HTTPRequestInputProtocol, OutputType: Decodable {
-            return try executeAsyncWithOutput(
-                endpointOverride: endpointOverride,
-                endpointPath: endpointPath,
-                httpMethod: httpMethod,
-                input: input,
-                completion: completion,
-                asyncResponseInvocationStrategy: GlobalDispatchQueueAsyncResponseInvocationStrategy<HTTPResult<OutputType>>(),
-                handlerDelegate: handlerDelegate)
+    public func close() {
+        // TODO: Move event loop shutdown from deinit
     }
-
+    
     /**
-     Submits a request that will return a response body to this client asynchronously.
-
-     - Parameters:
-     - endpointPath: The endpoint path for this request.
-     - httpMethod: The http method to use for this request.
-     - input: the input body data to send with this request.
-     - completion: Completion handler called with the response body or any error.
-     - asyncResponseInvocationStrategy: The invocation strategy for the response from this request.
-     - handlerDelegate: the delegate used to customize the request's channel handler.
+     Waits for the client to be closed. If stop() is not called,
+     this will block forever.
      */
-    public func executeAsyncWithOutput<InputType, OutputType, InvocationStrategyType>(
-            endpointOverride: URL? = nil,
-            endpointPath: String,
-            httpMethod: HTTPMethod,
-            input: InputType,
-            completion: @escaping (HTTPResult<OutputType>) -> (),
-            asyncResponseInvocationStrategy: InvocationStrategyType,
-            handlerDelegate: HTTPClientChannelInboundHandlerDelegate) throws -> Channel
-            where InputType: HTTPRequestInputProtocol, InvocationStrategyType: AsyncResponseInvocationStrategy,
-        InvocationStrategyType.OutputType == HTTPResult<OutputType>, OutputType: Decodable {
-
-        var hasComplete = false
-        let requestDelegate = clientDelegate
-        // create a wrapping completion handler to pass to the ChannelInboundHandler
-        // that will decode the returned body into the desired decodable type.
-        let wrappingCompletion: (HTTPResult<Data?>) -> () = { (rawResult) in
-            let result: HTTPResult<OutputType>
-
-            switch rawResult {
-            case .error(let error):
-                // its an error; complete with the provided error
-                result = .error(error)
-            case .response(let response):
-                // we are expecting a response body
-                guard let response = response else {
-                    // complete with a bad response error
-                    return completion(.error(HTTPError.badResponse("Unexpected empty response.")))
-                }
-
-                do {
-                    // decode the provided body into the desired type
-                    let output: OutputType = try requestDelegate.decodeOutput(output: response)
-
-                    // complete with the decoded type
-                    result = .response(output)
-                } catch {
-                    // if there was a decoding error, complete with that error
-                    result = .error(error)
-                }
-            }
-
-            asyncResponseInvocationStrategy.invokeResponse(response: result, completion: completion)
-            hasComplete = true
-        }
-
-        // submit the asynchronous request
-        let channel = try executeAsync(endpointOverride: endpointOverride,
-                                       endpointPath: endpointPath,
-                                       httpMethod: httpMethod,
-                                       input: input,
-                                       completion: wrappingCompletion,
-                                       handlerDelegate: handlerDelegate)
-
-        channel.closeFuture.whenComplete {
-            // if this channel is being closed and no response has been recorded
-            if !hasComplete {
-                completion(.error(HTTPClient.unexpectedClosureType))
-            }
-        }
-
-        return channel
-    }
-
-    /**
-     Submits a request that will not return a response body to this client asynchronously.
-     The completion handler's execution will be scheduled on DispatchQueue.global()
-     rather than executing on a thread from SwiftNIO.
-
-     - Parameters:
-     - endpointPath: The endpoint path for this request.
-     - httpMethod: The http method to use for this request.
-     - input: the input body data to send with this request.
-     - completion: Completion handler called with an error if one occurs or nil otherwise.
-     - handlerDelegate: the delegate used to customize the request's channel handler.
-     */
-    public func executeAsyncWithoutOutput<InputType>(
-            endpointOverride: URL? = nil,
-            endpointPath: String,
-            httpMethod: HTTPMethod,
-            input: InputType,
-            completion: @escaping (Error?) -> (),
-            handlerDelegate: HTTPClientChannelInboundHandlerDelegate) throws -> Channel
-            where InputType: HTTPRequestInputProtocol {
-                return try executeAsyncWithoutOutput(
-                    endpointOverride: endpointOverride,
-                    endpointPath: endpointPath,
-                    httpMethod: httpMethod,
-                    input: input,
-                    completion: completion,
-                    asyncResponseInvocationStrategy: GlobalDispatchQueueAsyncResponseInvocationStrategy<Error?>(),
-                    handlerDelegate: handlerDelegate)
-    }
-
-    /**
-     Submits a request that will not return a response body to this client asynchronously.
-
-     - Parameters:
-     - endpointPath: The endpoint path for this request.
-     - httpMethod: The http method to use for this request.
-     - input: the input body data to send with this request.
-     - completion: Completion handler called with an error if one occurs or nil otherwise.
-     - asyncResponseInvocationStrategy: The invocation strategy for the response from this request.
-     - handlerDelegate: the delegate used to customize the request's channel handler.
-     */
-    public func executeAsyncWithoutOutput<InputType, InvocationStrategyType>(
-            endpointOverride: URL? = nil,
-            endpointPath: String,
-            httpMethod: HTTPMethod,
-            input: InputType,
-            completion: @escaping (Error?) -> (),
-            asyncResponseInvocationStrategy: InvocationStrategyType,
-            handlerDelegate: HTTPClientChannelInboundHandlerDelegate) throws -> Channel
-            where InputType: HTTPRequestInputProtocol, InvocationStrategyType: AsyncResponseInvocationStrategy,
-            InvocationStrategyType.OutputType == Error? {
-
-        var hasComplete = false
-        // create a wrapping completion handler to pass to the ChannelInboundHandler
-        let wrappingCompletion: (HTTPResult<Data?>) -> () = { (rawResult) in
-            let result: Error?
-
-            switch rawResult {
-            case .error(let error):
-                // its an error, complete with this error
-                result = error
-            case .response:
-                // its a successful completion, complete with an empty error.
-                result = nil
-            }
-
-            asyncResponseInvocationStrategy.invokeResponse(response: result, completion: completion)
-            hasComplete = true
-        }
-
-        // submit the asynchronous request
-        let channel = try executeAsync(endpointOverride: endpointOverride,
-                                       endpointPath: endpointPath,
-                                       httpMethod: httpMethod,
-                                       input: input,
-                                       completion: wrappingCompletion,
-                                       handlerDelegate: handlerDelegate)
-
-        channel.closeFuture.whenComplete {
-            // if this channel is being closed and no response has been recorded
-            if !hasComplete {
-                completion(HTTPClient.unexpectedClosureType)
-            }
-        }
-
-        return channel
-    }
-
-    /**
-     Submits a request that will return a response body to this client synchronously.
-
-     - Parameters:
-     - endpointPath: The endpoint path for this request.
-     - httpMethod: The http method to use for this request.
-     - input: the input body data to send with this request.
-     - handlerDelegate: the delegate used to customize the request's channel handler.
-     - Returns: the response body.
-     - Throws: If an error occurred during the request.
-     */
-    public func executeSyncWithOutput<InputType, OutputType>(
-            endpointOverride: URL? = nil,
-            endpointPath: String,
-            httpMethod: HTTPMethod,
-            input: InputType,
-            handlerDelegate: HTTPClientChannelInboundHandlerDelegate) throws -> OutputType
-            where InputType: HTTPRequestInputProtocol, OutputType: Codable {
-
-        var responseResult: HTTPResult<OutputType>?
-        let completedSemaphore = DispatchSemaphore(value: 0)
-
-        let completion: (HTTPResult<OutputType>) -> () = { result in
-            responseResult = result
-            completedSemaphore.signal()
-        }
-
-        let channel = try executeAsyncWithOutput(endpointOverride: endpointOverride,
-                                                 endpointPath: endpointPath,
-                                                 httpMethod: httpMethod,
-                                                 input: input,
-                                                 completion: completion,
-                                                 // the completion handler can be safely executed on a SwiftNIO thread
-                                                 asyncResponseInvocationStrategy: SameThreadAsyncResponseInvocationStrategy<HTTPResult<OutputType>>(),
-                                                 handlerDelegate: handlerDelegate)
-
-        channel.closeFuture.whenComplete {
-            // if this channel is being closed and no response has been recorded
-            if responseResult == nil {
-                responseResult = .error(HTTPClient.unexpectedClosureType)
-                completedSemaphore.signal()
-            }
-        }
-
-        Log.verbose("Waiting for response from \(endpointOverride?.host ?? endpointHostName) ...")
-        completedSemaphore.wait()
-
-        guard let result = responseResult else {
-            throw HTTPError.connectionError("Http request was closed without returning a response.")
-        }
-
-        Log.verbose("Got response from \(endpointOverride?.host ?? endpointHostName) - response received: \(result)")
-
-        switch result {
-        case .error(let error):
-            throw error
-        case .response(let response):
-            return response
-        }
-    }
-
-    private struct AsyncErrorResult {
-        let error: Error?
-    }
-
-    /**
-     Submits a request that will not return a response body to this client synchronously.
-
-     - Parameters:
-     - endpointPath: The endpoint path for this request.
-     - httpMethod: The http method to use for this request.
-     - input: the input body data to send with this request.
-     - handlerDelegate: the delegate used to customize the request's channel handler.
-     - Throws: If an error occurred during the request.
-     */
-    public func executeSyncWithoutOutput<InputType>(
-            endpointOverride: URL? = nil,
-            endpointPath: String,
-            httpMethod: HTTPMethod,
-            input: InputType,
-            handlerDelegate: HTTPClientChannelInboundHandlerDelegate) throws
-            where InputType: HTTPRequestInputProtocol {
-        var responseError: AsyncErrorResult?
-        let completedSemaphore = DispatchSemaphore(value: 0)
-
-        let completion: (Error?) -> () = { error in
-            responseError = AsyncErrorResult(error: error)
-            completedSemaphore.signal()
-        }
-
-        let channel = try executeAsyncWithoutOutput(endpointOverride: endpointOverride,
-                                                    endpointPath: endpointPath,
-                                                    httpMethod: httpMethod,
-                                                    input: input,
-                                                    completion: completion,
-                                                    // the completion handler can be safely executed on a SwiftNIO thread
-                                                    asyncResponseInvocationStrategy: SameThreadAsyncResponseInvocationStrategy<Error?>(),
-                                                    handlerDelegate: handlerDelegate)
-
-        channel.closeFuture.whenComplete {
-            // if this channel is being closed and no response has been recorded
-            if responseError == nil {
-                responseError = AsyncErrorResult(error: HTTPClient.unexpectedClosureType)
-                completedSemaphore.signal()
-            }
-        }
-
-        completedSemaphore.wait()
-
-        if let error = responseError?.error {
-            throw error
-        }
+    public func wait() {
+        
     }
 
     func executeAsync<InputType>(
@@ -388,7 +97,7 @@ public class HTTPClient {
             endpointPath: String,
             httpMethod: HTTPMethod,
             input: InputType,
-            completion: @escaping (HTTPResult<Data?>) -> (),
+            completion: @escaping (HTTPResult<HTTPResponseComponents>) -> (),
             handlerDelegate: HTTPClientChannelInboundHandlerDelegate) throws -> Channel
             where InputType: HTTPRequestInputProtocol {
 
--- Sources/SmokeHTTPClient/HTTPClientChannelInboundHandler.swift
+++ Sources/SmokeHTTPClient/HTTPClientChannelInboundHandler.swift
@@ -56,9 +56,9 @@ public final class HTTPClientChannelInboundHandler: ChannelInboundHandler {
     public var bodyParts: [ByteBuffer] = []
 
     /// A completion handler to pass any recieved response to.
-    private let completion: (HTTPResult<Data?>) -> ()
+    private let completion: (HTTPResult<HTTPResponseComponents>) -> ()
     /// A function that provides an Error based on the payload provided.
-    private let errorProvider: (HTTPResponseHead, Data) throws -> Error
+    private let errorProvider: (HTTPResponseHead, HTTPResponseComponents) throws -> Error
     /// Delegate that provides client-specific logic
     private let delegate: HTTPClientChannelInboundHandlerDelegate
 
@@ -81,8 +81,8 @@ public final class HTTPClientChannelInboundHandler: ChannelInboundHandler {
          httpMethod: HTTPMethod,
          bodyData: Data,
          additionalHeaders: [(String, String)],
-         errorProvider: @escaping (HTTPResponseHead, Data) throws -> Error,
-         completion: @escaping (HTTPResult<Data?>) -> (),
+         errorProvider: @escaping (HTTPResponseHead, HTTPResponseComponents) throws -> Error,
+         completion: @escaping (HTTPResult<HTTPResponseComponents>) -> (),
          channelInboundHandlerDelegate: HTTPClientChannelInboundHandlerDelegate) {
         self.contentType = contentType
         self.endpointUrl = endpointUrl
@@ -118,6 +118,14 @@ public final class HTTPClientChannelInboundHandler: ChannelInboundHandler {
             // handle this response
             handleCompleteResponse(context: ctx)
         }
+    }
+    
+    private func getHeadersFromResponse(header: HTTPResponseHead) -> [(String, String?)] {
+        let headers: [(String, String?)] = header.headers.map { header in
+            return (header.name, header.value)
+        }
+        
+        return headers
     }
 
     /*
@@ -158,6 +166,10 @@ public final class HTTPClientChannelInboundHandler: ChannelInboundHandler {
             completion(.error(error))
             return
         }
+        
+        let headers = getHeadersFromResponse(header: responseHead)
+        let responseComponents = HTTPResponseComponents(headers: headers,
+                                                        body: responseBodyData)
 
         if let bodyData = responseBodyData {
             Log.verbose("Got response from endpoint: \(endpointUrl) and path: \(endpointPath) with " +
@@ -170,7 +182,7 @@ public final class HTTPClientChannelInboundHandler: ChannelInboundHandler {
         // if the response status is ok
         if case .ok = responseHead.status {
             // complete with the response data (potentially empty)
-            completion(.response(responseBodyData))
+            completion(.response(responseComponents))
             return
         }
 
@@ -180,17 +192,10 @@ public final class HTTPClientChannelInboundHandler: ChannelInboundHandler {
             return
         }
 
-        // otherwise this is the error case
-        // If there is no body, can't know anything about the error
-        guard let bodyData = responseBodyData else {
-            completion(.error(HTTPError.unknownError("Error with status '\(responseHead.status)' with empty body")))
-            return
-        }
-
         let responseError: Error
         do {
             // attempt to get the error from the provider
-            responseError = try errorProvider(responseHead, bodyData)
+            responseError = try errorProvider(responseHead, responseComponents)
         } catch {
             // if the provider throws an error, use this error
             responseError = error
